af3cb3acf69f6eeed47eb3072a0a96992b1ca524
kanatsanan6 <kanatsanan.j@datawow.io>
2023-10-06 22:16:11 +0700

    build: setup env

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] })],
 })
 export class AppModule {}
---
+++ api/src/config/database.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT) || 5432,
+  user: process.env.DATABASE_USER || 'postgres',
+  pass: process.env.DATABASE_PASS || 'postgres',
+  name: process.env.DATABASE_NAME || 'certify_development',
+}));
